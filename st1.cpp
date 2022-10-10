3e80eb919eda98374fc135185db449c8c1ab6992
MrPranay <83683295+MrPranay@users.noreply.github.com>
2022-10-10 13:02:01 +0530

    Add files via upload

---
+++ st1.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define FOR(x,i,e) for(int x=i; x<e; x++)
+#define pb push_back
+#define vi vector<int>
+#define pii pair<int,int>
+#define double long double
+#define all(x) (x).begin(),(x).end()
+
+
+
+signed main()
+{
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif*/
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int r1,w1,c1,r2,w2,c2;
+		cin>>r1>>w1>>c1;
+        cin>>r2>>w2>>c2;
+        if ( ((r1>r2) && (w1>w2)) || ((r1>r2) && (c1>c2)) || ((w1>w2) && (c1>c2)) ) cout<<'A'<<endl;
+        
+        else cout<<'B'<<endl;
+	}
+
+
+	return 0;
+}
---
+++ stackll.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define CAPACITY 10000
+struct stack 
+{
+    int data;
+    struct stack *next;
+} *top;
+
+int size = 0;
+
+
+void push(int element)
+{
+    if (size >= CAPACITY)
+    {
+        printf("Stack Overflow, can't add more element to stack.\n");
+        return;
+    }
+    struct stack * newNode = (struct stack *) malloc(sizeof(struct stack));
+    newNode->data = element;
+    newNode->next = top;        
+    top = newNode;
+    size++;
+
+    printf("%d pushed to stack.\n",element);
+}
+int pop()
+{
+    int data = 0;
+    struct stack * topNode;
+    if (size <= 0 || !top)
+    {
+        printf("Stack is empty.\n");
+        return INT_MIN;
+    }
+    topNode = top;
+    data = top->data;
+    top = top->next;
+    free(topNode);
+    size--;
+
+    return data;
+}
+void display(){
+    struct stack *temp;
+    if(top == NULL)
+    {
+        printf("Stack is empty.");
+        return;
+    }
+    
+    temp = top;
+    while(temp != NULL){
+        printf(" %d ", temp->data); 
+        temp = temp->next;                 
+    }
+}
+int main()
+{
+    push(8);
+    push(9);
+    push(6);
+    push(4);
+    push(1);
+    pop();
+    display();
+    return 0;
+}
